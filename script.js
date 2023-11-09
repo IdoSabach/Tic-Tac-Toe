a4fb93783bc772f95525329696b2c062e086b8e8
Ido <Idosabach11@gmail.com>
2023-11-09 23:13:58 +0200

    test to js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const box = document.createElement('button')
 box.classList.add('box')
 box.textContent = "ido"
 ido.appendChild(box)
+
+function Book(name,page,year){
+  this.name = name
+  this.page = page
+  this.year = year
+  this.info = ()=>{console.log(name + " " + page + " " + year)}
+}
+
+let arr =[]
+for(let i =0 ; i<5; i++){
+  let book = new Book('ido',100+(i*9),2000+(i+10))
+  console.log(book.info())
+
+}
